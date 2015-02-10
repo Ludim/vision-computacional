93a5c6b0054aec5d75d22a4d78fd631bd11ac653
Ludim <ludim.anel@gmail.com>
2015-02-10 03:42:49 -0600

    carpeta tarea1

--- tarea1/operadores.py
+++ tarea1/operadores.py
@@ -4,6 +4,7 @@
 from PIL import Image
 from sys import argv
 import math
+from matplotlib.pylab import hist, show
 '''
 Clase que tiene los operadores encargados para la tarea 1:
   Operaciones:
@@ -33,6 +34,40 @@ class Operador():
         self.h = image.size[1]
         self.newIm = Image.new(self.i.mode, self.i.size)
     
+    # TOMAR UNA IMAGEN ESCALA DE GRISES Y VER QUE TANTO AFECTA
+    def elongacion(self):
+        # primero obtener el pixel mas grande y el mas pequeno
+        matrizPix = self.i.load() 
+        listaPixeles = []
+        frecuencias = {}
+        for i in xrange(0,256):
+            frecuencias[i] = 0
+
+        for x in xrange(self.w):
+            for y in xrange(self.h):
+                #colorPromedio = (matrizPix[x,y][0] + matrizPix[x,y][1] + matrizPix[x,y][2])/3
+                colorPromedio = matrizPix[x,y][0]
+                listaPixeles.append(colorPromedio)                
+                #print colorPromedio
+                if colorPromedio in frecuencias:                    
+                    frecuencias[colorPromedio] = frecuencias[colorPromedio] + 1
+        print "FRECUENCIAS"
+        # http://progpython.blogspot.mx/2011/09/histogramas-con-python-matplotlib.html
+        # http://pythonya.appspot.com/detalleconcepto?deta=Los%20diccionarios%20tratados%20como%20objetos
+        print frecuencias.values()
+        print
+        hist(frecuencias.values(), 256,(0,255))
+        show()
+            
+
+        print frecuencias
+        pixMenor = min(listaPixeles)
+        pixMayor = max(listaPixeles)
+        print "Pix menor ", pixMenor
+        print "Pix mayor ",pixMayor
+
+
+
 
     # def shiftingHorizontal(self):
     #     # primero saco la media de cada columna
@@ -66,7 +101,8 @@ def main():
     print "My image: ", nameIm
     operador = Operador(Image.open(nameIm))
     #operador.shiftingHorizontal()
-    operador.umbral()
+    #operador.umbral()
+    operador.elongacion()
 
 
 if __name__ == '__main__':
