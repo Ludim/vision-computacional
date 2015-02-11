02462653cd3af90cb5b7c82d12f88318c642e754
Ludim <ludim.anel@gmail.com>
2015-02-11 00:21:25 -0600

    carpeta tarea1

--- tarea1/operadores.py
+++ tarea1/operadores.py
@@ -5,6 +5,8 @@ from PIL import Image
 from sys import argv
 import math
 from matplotlib.pylab import hist, show
+import cv
+from numpy import *
 '''
 Clase que tiene los operadores encargados para la tarea 1:
   Operaciones:
@@ -12,20 +14,19 @@ Clase que tiene los operadores encargados para la tarea 1:
         - Shifting horizontal
         - Shifting vertical
         - Shifting diagonal
-        - Oscurecer una imagen
-        - Presentar una imagen con su reflejo. 
-        - Presentar uan imagen con su inversion vertical-horizontal (esto no es voltear la imagen vertica horizontal??, NO EL EFECTO ESPEJO HORIZONTAL VERTICAL)
+        - Oscurecer una imagen (ya)
+        - Presentar una imagen con su reflejo.(ya)
+        - Presentar uan imagen con su inversion vertical-horizontal (esto no es voltear la imagen vertica horizontal??, NO EL EFECTO ESPEJO HORIZONTAL VERTICAL) (ya???)
      Grupal
       Permitir al usuario elegir entre diferentes operaciones
         - Aclarado (ya)
         - Copiado (ya)
         - Escala de grises (ya)
         - Imagen binaria (ya) => umbral???
-        - Umbral (ver diapositiva 47) es para obtener bordes
+        - Umbral (ver diapositiva 47) es para obtener bordes (ya)
         - Negativo (ya)
         - Elongación o contraste de una imagen*  (cuando se tiene un rango ej 100 - 125 a 50 a 200) 
              Q0 = P0 *gamma + beta
-diferencia entre aumento de brillo y aclarado de una imagen?
 '''
 class Operador():
     def __init__(self,image):
@@ -39,9 +40,9 @@ class Operador():
         # primero obtener el pixel mas grande y el mas pequeno
         matrizPix = self.i.load() 
         listaPixeles = []
-#        frecuencias = {}
-#        for i in xrange(0,256):
-#            frecuencias[i] = 0
+        frecuencias = {}
+        for i in xrange(0,256):
+            frecuencias[i] = 0
 
         for x in xrange(self.w):
             for y in xrange(self.h):
@@ -49,48 +50,121 @@ class Operador():
                 #colorPromedio = matrizPix[x,y][0]
                 listaPixeles.append(colorPromedio)
                 #print colorPromedio
-                # if colorPromedio in frecuencias:                    
-                #     frecuencias[colorPromedio] = frecuencias[colorPromedio] + 1
-        # print "FRECUENCIAS"
-        # # http://progpython.blogspot.mx/2011/09/histogramas-con-python-matplotlib.html
-        # # http://pythonya.appspot.com/detalleconcepto?deta=Los%20diccionarios%20tratados%20como%20objetos
-        # print frecuencias.values()
-        # print
-        # hist(frecuencias.values(), 256,(0,255))
-        # show()
+                if colorPromedio in frecuencias:
+                     frecuencias[colorPromedio] = frecuencias[colorPromedio] + 1
+        print "FRECUENCIAS"
+        #http://progpython.blogspot.mx/2011/09/histogramas-con-python-matplotlib.html
+        #http://pythonya.appspot.com/detalleconcepto?deta=Los%20diccionarios%20tratados%20como%20objetos
+        print frecuencias.values()
+        print
+        hist(frecuencias.values(), 256,(0,255))
+        show()
             
-
-        #print frecuencias
+        print frecuencias
         pixMenor = min(listaPixeles)
         pixMayor = max(listaPixeles)
         print "Pix menor ", pixMenor
         print "Pix mayor ",pixMayor
         
         # hay que ver como cambia el histograma
+        #gamma = pixMayor + 150
+        #beta = pixMenor + 1
+        #gamma  = 10
+        #beta = 50
+        scale_factor = 255 / (pixMayor - pixMenor)
+        #http://homepages.inf.ed.ac.uk/rbf/BOOKS/VERNON/Chap004.pdf
         for x in xrange(self.w):
             for y in xrange(self.h):
                 r,g,b = self.i.getpixel((x,y))
-                #print r,g,b
-                r = ((r - pixMenor) / (pixMayor - pixMenor)) * r#255
-                g = ((g - pixMenor) / (pixMayor - pixMenor)) * g#255
-                b = ((b - pixMenor) / (pixMayor - pixMenor)) * b#255
-                #print r,g,b
+                r = int((int((r + pixMayor) * 2.55) + int((r - pixMenor) * 2.55))/2)
+                g = int((int((g + pixMayor) * 2.55) + int((g - pixMenor) * 2.55))/2)
+                b = int((int((b + pixMayor) * 2.55) + int((b - pixMenor) * 2.55))/2)
+#                g = int((g + pixMayor) * 2.55)
+#                b = int((b + pixMayor) * 2.55)
+                #r = (r * gamma + beta)/255
+                #g = (g * gamma + beta)/255
+                #b = (b * gamma + beta)/255
+                if r <= 0 or g <= 0 or b <= 0:
+                    r,g,b = 0,0,0
+                elif r >= 255 or g >= 255 or b >= 255:
+                    r,g,b = 255,255,255
                 self.newIm.putpixel((x,y),(r,g,b))
+                
         self.newIm.save("fldrimg/elonga", self.i.format)
-        
+        matrizPix = self.newIm.load() 
+        listaPixeles = []
+        frecuencias = {}
+        for i in xrange(0,256):
+            frecuencias[i] = 0
 
-    # def shiftingHorizontal(self):
-    #     # primero saco la media de cada columna
-    #     for x in xrange(self.w):
-    #         for y in xrange(self.h):
-    #             r,g,b = self.i.getpixel((x,y))
-    #             self.i.putpixel((x,y),(r,g,b))
-    #     self.newIm.save("fldrimg/izq",self.i.format)
+        for x in xrange(self.w):
+            for y in xrange(self.h):
+                colorPromedio = (matrizPix[x,y][0] + matrizPix[x,y][1] + matrizPix[x,y][2])/3
+                listaPixeles.append(colorPromedio)
+                if colorPromedio in frecuencias:
+                     frecuencias[colorPromedio] = frecuencias[colorPromedio] + 1
+        print "FRECUENCIAS"
+        print frecuencias.values()
+        print
+        hist(frecuencias.values(), 256,(0,255))
+        show()
+            
+        print frecuencias
 
-    # def shiftingVertical(self,mover,cuantosPixeles):
-    #     print 'hola'
-    #     return 1
+    def shiftHor(self):
+        # primero mejor intenter con una matriz pequena
+        # 
+        centro = int(math.floor((self.w)/2))
+        print centro
+        # primero obtener las medias de cada columna
+        matriz = self.i.load()
+        mediaCol = []
+        for y in xrange(self.h):
+            media = 0
+            for x in xrange(self.w):
+                media = ((matriz[x,y][0] + matriz[x,y][1] + matriz[x,y][2]) / 3) + media
+            media = media / self.w
+            mediaCol.append(media)
+            #print media
+        print "Media de cada columna: ",mediaCol 
+        print len(mediaCol)
+
         
+        for y in xrange(self.h):
+            for x in xrange(self.w): # recorre para abajo
+                # obtener pixel de la otra esquina
+                r,g,b = self.i.getpixel((self.w-10-y,x))
+                r = (mediaCol[y] * r) / mediaCol[len(mediaCol)-y-1]
+                g = (mediaCol[y] * g) / mediaCol[len(mediaCol)-y-1]
+                b = (mediaCol[y] * b) / mediaCol[len(mediaCol)-y-1]
+                self.newIm.putpixel((x,y),(r,g,b))
+        self.newIm.save("fldrimg/shifth",self.i.format)
+
+
+    def mirrorHorizontal(self):
+        w = self.w * 2
+        h = self.h
+        newIm = Image.new(self.i.mode, (w,h))        
+        matriz = self.i.load()
+        for x in xrange(self.w):
+            for y in xrange(self.h):
+                r = matriz[x,y][0]
+                g = matriz[x,y][1]
+                b = matriz[x,y][2]
+                newIm.putpixel((x,y),(r,g,b))
+                newIm.putpixel((w-x-1,y),(r,g,b))
+        newIm.save("fldrimg/espejo",self.i.format)
+
+    def mirrorVertical(self):
+        matriz = self.i.load()
+        for x in xrange(self.w):
+            for y in xrange(self.h):
+                r = matriz[x,y][0]
+                g = matriz[x,y][1]
+                b = matriz[x,y][2]
+                self.newIm.putpixel((self.w-x-1,self.h-y-1),(r,g,b))
+        self.newIm.save("fldrimg/vertical",self.i.format)
+
     def umbral(self):
         print 'umbral'
         UMBRAL = 127 
@@ -102,8 +176,6 @@ class Operador():
                 else:
                     self.newIm.putpixel((x,y),(0,0,0))
         self.newIm.save("fldrimg/umbral",self.i.format)
-                    
-
 
 
 def main():
@@ -112,8 +184,9 @@ def main():
     operador = Operador(Image.open(nameIm))
     #operador.shiftingHorizontal()
     #operador.umbral()
-    operador.elongacion()
-
-
+    #operador.elongacion()
+    #operador.shiftHor()
+    operador.mirrorHorizontal()
+    operador.mirrorVertical()
 if __name__ == '__main__':
   main()
