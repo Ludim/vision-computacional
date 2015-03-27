bb666fd684a46d6cee5d9f39e89568947d5d0f05
Ludim <ludim.anel@gmail.com>
2015-03-26 21:22:36 -0600

    zonas de vecindad

--- tarea2/src/vecindad.py
+++ tarea2/src/vecindad.py
@@ -2,35 +2,46 @@
 from PIL import Image
 import random
 
+# poner el codigo en espanol, es decir variables en espanol no mezclado :P
+
+#olvida la imagen y crea la matriz a partir de un archivo de texto
+pixelesImagen = []
+
+i = 0
+with open("chitaoutput.txt","r") as archivo: # este archivo se genera cuando se umbraliza la imagen
+	for linea in archivo:
+		j = 0
+		pixelesImagen.append([])
+		for pixel in linea.split(","):
+			if pixel != ' \n':
+				pixelesImagen[i].append(int(pixel))
+			j+=1
+		i+=1
+
+
 img = Image.open("../img/umbral")
-matriz = img.load()
-width = img.size[0]
-height = img.size[1]
-new = Image.new(img.mode,img.size)
+#matriz = img.load()
+anchoImag = img.size[0]
+altoImag = img.size[1]
+nuevaImag = Image.new(img.mode,img.size)
 visitados = []
 vecindad = [] # lista de listas
 aux = []
 
-colores = []
-for i in range(width):
-	for j in range(height):
-		print img.getpixel((i,j))
-		print matriz[i,j][0], ",",matriz[i,j][1],",",matriz[i,j][2]
-		#colores.append(matriz[i,j][0])
-#a = colores.sort()
-#print colores
-exit()
 
-for w in range(width):
-	for h in range(height):
+#for w in range(width):
+for w in range(anchoImag):
+	for h in range(altoImag):
+	#for h in range(height):
 #		print w,h
 		if not((w,h) in visitados):
 			vecindad = []
 			siguiente = [(w,h)]
-			colPix = matriz[w,h][0] # color pixel
+			#colPix = matriz[w,h][0] # color pixel
+			colPix = pixelesImagen[w][h] # color pixel
 			print "Color ",colPix
 			nuevocolor = random.randint(0,255)
-			new.putpixel((w,h),(nuevocolor,nuevocolor,nuevocolor))
+			nuevaImag.putpixel((w,h),(nuevocolor,nuevocolor,nuevocolor))
 			while siguiente:
 				posPix = siguiente.pop() # posicion pixel
 				x = posPix[0]
@@ -42,16 +53,18 @@ for w in range(width):
 					for j in range(y-1,y+2):
 						if not(x == i and y == j):
 							if ((i >= 0 and j >= 0) and
-								(i < width and j < height) 
-								and (colPix == matriz[i,j][0])):
+								(i < anchoImag and j < altoImag) 
+								and (colPix == pixelesImagen[i][j])):
+								#and (colPix == matriz[i,j][0])):
 								posPix = (i,j)
-								print "Color ",colPix, " Color vecino ", matriz[i,j][0]
+								#print "Color ",colPix, " Color vecino ", matriz[i,j][0]
+								print "Color ",colPix, " Color vecino ", pixelesImagen[i][j]
 								if (not(posPix in vecindad) and not(posPix in visitados)):
-									new.putpixel(posPix,(nuevocolor,nuevocolor,nuevocolor))
+									nuevaImag.putpixel(posPix,(nuevocolor,nuevocolor,nuevocolor))
 									vecindad.append((i,j))
 									siguiente.append((i,j))
 			print vecindad
 			aux.append(vecindad)
-new.save("../img/umbral_VECINDAD", img.format)
+nuevaImag.save("../img/umbral_VECINDAD", img.format)
 for i in range(len(aux)):
 	print aux[i]
