6e6394b14454fdcc81c2b824c7816a284767a40d
Ludim <ludim.anel@gmail.com>
2015-03-27 00:02:52 -0600

    vecindades

--- tarea2/src/vecindad.py
+++ tarea2/src/vecindad.py
@@ -6,7 +6,7 @@ import random
 pixelesImagen = []
 
 i = 0
-with open("image_lines.txt","r") as archivo: # este archivo se genera cuando se umbraliza la imagen
+with open("said.txt","r") as archivo: # este archivo se genera cuando se umbraliza la imagen
 	for linea in archivo:
 		j = 0
 		pixelesImagen.append([])
@@ -16,22 +16,20 @@ with open("image_lines.txt","r") as archivo: # este archivo se genera cuando se 
 			j+=1
 		i+=1
 
-
 img = Image.open("../img/said_umbral.jpg")
 #matriz = img.load()
 anchoImag = img.size[0]
 altoImag = img.size[1]
 nuevaImag = Image.new(img.mode,img.size)
 visitados = []
-vecindad = [] # lista de listas
-aux = []
-
+#vecindad = [] # lista de listas
+aux = [] # vecindarios
 
 #for w in range(width):
 for w in range(anchoImag):
 	for h in range(altoImag):
 	#for h in range(height):
-#		print w,h
+		print w,h
 		if not((w,h) in visitados):
 			vecindad = []
 			siguiente = [(w,h)]
@@ -40,6 +38,7 @@ for w in range(anchoImag):
 			print "Color ",colPix
 			nuevocolor = random.randint(0,255)
 			nuevaImag.putpixel((w,h),(nuevocolor,nuevocolor,nuevocolor))
+			#agregar linea para el archivo que guarde este nuevo color
 			while siguiente:
 				posPix = siguiente.pop() # posicion pixel
 				x = posPix[0]
@@ -47,6 +46,7 @@ for w in range(anchoImag):
 				visitados.append(posPix)
 				if not(posPix in vecindad):
 					vecindad.append(posPix)
+				# revisa vecinos cercanos
 				for i in range(x-1,x+2):
 					for j in range(y-1,y+2):
 						if not(x == i and y == j):
@@ -56,13 +56,14 @@ for w in range(anchoImag):
 								#and (colPix == matriz[i,j][0])):
 								posPix = (i,j)
 								#print "Color ",colPix, " Color vecino ", matriz[i,j][0]
-								print "Color ",colPix, " Color vecino ", pixelesImagen[i][j]
+								#print "Color ",colPix, " Color vecino ", pixelesImagen[i][j]
 								if (not(posPix in vecindad) and not(posPix in visitados)):
 									nuevaImag.putpixel(posPix,(nuevocolor,nuevocolor,nuevocolor))
+									# agregar linea para guardar en archivo						
 									vecindad.append((i,j))
 									siguiente.append((i,j))
-			print vecindad
+			#print vecindad
 			aux.append(vecindad)
-nuevaImag.save("../img/said_VECINDAD.png", img.format)
+nuevaImag.save("../img/said_VECINDAD.jpg", img.format)
 for i in range(len(aux)):
 	print aux[i]
